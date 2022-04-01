fe22b912efe0a50759e2cf297ad5d820671b5b2b
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2022-04-01 19:28:49 +0530

    Create myspark-example.py

---
+++ airflow/dags/myspark-example.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from airflow.models import DAG
+from airflow.providers.apache.spark.operators.spark_jdbc import SparkJDBCOperator
+from airflow.providers.apache.spark.operators.spark_sql import SparkSqlOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+with DAG(
+    dag_id='myspark_example_dag',
+    schedule_interval=None,
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['spark_example'],
+) as dag:
+    # [START howto_operator_spark_submit]
+    submit_job1 = SparkSubmitOperator(
+        application="/home/ubuntu/airflow/examples/spark-example1.py", task_id="submit_job1"
+    )
+    # [END howto_operator_spark_submit]
+ 
+    submit_job2 = SparkSubmitOperator(
+        application="/home/ubuntu/airflow/examples/spark-example1.py", task_id="submit_job2"
+    )
+
+
+    submit_job1 >> submit_job2
